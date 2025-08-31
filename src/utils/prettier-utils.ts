1321fbfe81dd9280382374c696f2cee8c944329d
Yossi Saadi <yossisa@monday.com>
2025-08-31 15:12:33 +0300

    chore: revet package changes
    It is out of scope for this PR, let's handle it in another one

--- src/utils/prettier-utils.ts
+++ src/utils/prettier-utils.ts
@@ -1,32 +1,31 @@
 import { format, Options } from "prettier";
-import parserHtml from "prettier/plugins/html";
-import pluginEstree from "prettier/plugins/estree";
-import pluginTypescript from "prettier/plugins/typescript";
-import pluginPostcss from "prettier/plugins/postcss";
+import parserHtml from "prettier/parser-html";
+import parserTypeScript from "prettier/parser-typescript";
+import parserPostCss from "prettier/parser-postcss";
 
 const jsxOptions: Options = {
   parser: "typescript",
   arrowParens: "avoid",
   trailingComma: "es5",
-  plugins: [pluginEstree, parserHtml, pluginTypescript],
+  plugins: [parserHtml, parserTypeScript],
 };
 
 const cssOptions: Options = {
-  parser: "scss",
-  plugins: [pluginPostcss],
+  parser: "css",
+  plugins: [parserPostCss],
 };
 
-export async function formatJsx(code: string): Promise<string> {
+export function formatJsx(code: string): string {
   return formatCode(code, jsxOptions);
 }
 
-export async function formatCss(code: string): Promise<string> {
+export function formatCss(code: string): string {
   return formatCode(code, cssOptions);
 }
 
-async function formatCode(code: string, options: Options): Promise<string> {
+function formatCode(code: string, options: Options): string {
   try {
-    return (await format(code, options)).replace(/;\s*$/, "");
+    return format(code, options).replace(/;\s*$/, "");
   } catch (e) {
     throw new Error(`[Playground Error]: Error formatting code: ${e}`);
   }
